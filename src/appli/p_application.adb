d9fb863bee860aafec22a34f931ccc785f3bfac2
Mickaël <mickael.bourgier@gmail.com>
2012-05-14 13:58:36 +0200

    Indentation et clarification du code de p_application.ad*

--- src/appli/p_application.adb
+++ src/appli/p_application.adb
@@ -13,123 +13,114 @@ with ada.containers;
 
 package body p_application is
 
-procedure vider_tables is
-criteria : db_commons.Criteria;
-begin
-	festival_io.delete (criteria);
-	programme_jour_festival_io.delete (criteria);
-	participant_festival_io.delete (criteria);
-	groupe_io.delete (criteria);
-	jour_festival_io.delete (criteria);
-	ville_io.delete (criteria);
-end vider_tables;
+	procedure vider_tables is
+		criteria : db_commons.Criteria;
+	begin
+		festival_io.delete (criteria);
+		programme_jour_festival_io.delete (criteria);
+		participant_festival_io.delete (criteria);
+		groupe_io.delete (criteria);
+		jour_festival_io.delete (criteria);
+		ville_io.delete (criteria);
+	end vider_tables;
 
-procedure retrouver_villes(ensV : out ville_List.Vector ) is
-c  : db_commons.Criteria;
-
-begin
--- fixe l'ordre du résultat par ordre alphabétique des noms de ville
-  ville_io.add_nom_ville_to_orderings (c, asc);
-  ensV := ville_io.retrieve( c );
-  if ville_io.is_empty(ensV) then raise ExAucuneVille ;end if;
-end retrouver_villes;
-
-
-procedure retrouver_villes_avec_programme (ensVP : out based108_data.ville_List.Vector ) is
-ensFest : festival_List.Vector;
-c : db_commons.Criteria;
-
--- conserve dans ensVP les villes pour lesquelles un festival est entièrement programmé
-procedure verifie_prog (pos : festival_list.cursor) is
-c , c1, c2 ,c3 : db_commons.Criteria;
-n1, n2 : integer;
-fest : tfestival;
-ensJour : Jour_Festival_List.vector;
-j1, j2 : tjour_festival;  -- les 2 journées d'un festival
-ensProg1, ensProg2 : Programme_Jour_Festival_List.Vector;-- programme des 2 journées
-ensGroupesInscrits : participant_festival_list.vector;
-ville : tville;
-
-begin
-  fest := festival_List.element( pos );
-  -- recherche des journées du festival de la ville
-  ensJour := festival_io.Retrieve_Associated_Jour_Festivals( fest);
-
-  j1 := Jour_Festival_List.element(ensJour, Jour_Festival_List.first_index(ensJour));
--- recherche du programme de la journée 1
-  ensProg1 := jour_festival_io.Retrieve_Associated_Programme_Jour_Festivals(j1);
-
-  j2 := Jour_Festival_List.element(ensJour, Jour_Festival_List.last_index(ensJour));
--- recherche du programme de la journée 2
-  programme_jour_festival_io.Add_Jour_Fest( c2 , j2.id_jour_festival);
-  ensProg2 := programme_jour_festival_io.retrieve (c2);
-
--- recherche des groupes inscrits au festival de la ville
-  ensGroupesInscrits := festival_io.Retrieve_Associated_Participant_Festivals(fest);
-
--- n1 : nombre de groupes programmés total sur les 2 jours
-  n1 := integer(programme_jour_festival_io.card (ensProg1)) + integer(programme_jour_festival_io.card(ensProg2));
-
---n2 : nombre de groupes inscrits
-  n2 :=  integer(participant_festival_io.card(ensGroupesInscrits));
-
--- teste si le festival est entièrement programmé et ajoute la ville dans ensV
-  if n1 = n2 then
-	ville.nom_ville := fest.ville_festival;
-	ville_list.append (ensVP, ville);
-  end if;
-
-end verifie_prog;
+	procedure retrouver_villes(ensV : out ville_List.Vector ) is
+		c : db_commons.Criteria;
+	begin
+		-- fixe l'ordre du résultat par ordre alphabétique des noms de ville
+		ville_io.add_nom_ville_to_orderings (c, asc);
+		ensV := ville_io.retrieve( c );
+		if ville_io.is_empty(ensV) then raise ExAucuneVille ;end if;
+	end retrouver_villes;
 
 
-begin
-  ensFest := festival_io.retrieve (c);
-  festival_list.iterate (ensFest, verifie_prog'Access);
-  if ville_List.is_empty(ensVP)then raise ExAucuneVille ;
-  end if ;
+	procedure retrouver_villes_avec_programme (ensVP : out based108_data.ville_List.Vector ) is
+		ensFest : festival_List.Vector;
+		c : db_commons.Criteria;
+		-- conserve dans ensVP les villes pour lesquelles un festival est entièrement programmé
+		procedure verifie_prog (pos : festival_list.cursor) is
+			c , c1, c2 ,c3 : db_commons.Criteria;
+			n1, n2 : integer;
+			fest : tfestival;
+			ensJour : Jour_Festival_List.vector;
+			j1, j2 : tjour_festival;  -- les 2 journées d'un festival
+			ensProg1, ensProg2 : Programme_Jour_Festival_List.Vector;-- programme des 2 journées
+			ensGroupesInscrits : participant_festival_list.vector;
+			ville : tville;
+		begin
+			fest := festival_List.element( pos );
+			-- recherche des journées du festival de la ville
+			ensJour := festival_io.Retrieve_Associated_Jour_Festivals(fest);
 
-end retrouver_villes_avec_programme;
+			j1 := Jour_Festival_List.element(ensJour, Jour_Festival_List.first_index(ensJour));
+			-- recherche du programme de la journée 1
+			ensProg1 := jour_festival_io.Retrieve_Associated_Programme_Jour_Festivals(j1);
+
+			j2 := Jour_Festival_List.element(ensJour, Jour_Festival_List.last_index(ensJour));
+			-- recherche du programme de la journée 2
+			programme_jour_festival_io.Add_Jour_Fest(c2, j2.id_jour_festival);
+			ensProg2 := programme_jour_festival_io.retrieve (c2);
+
+			-- recherche des groupes inscrits au festival de la ville
+			ensGroupesInscrits := festival_io.Retrieve_Associated_Participant_Festivals(fest);
+
+			-- n1 : nombre de groupes programmés total sur les 2 jours
+			n1 := integer(programme_jour_festival_io.card (ensProg1)) + integer(programme_jour_festival_io.card(ensProg2));
+
+			--n2 : nombre de groupes inscrits
+			n2 :=  integer(participant_festival_io.card(ensGroupesInscrits));
+
+			-- teste si le festival est entièrement programmé et ajoute la ville dans ensV
+			if n1 = n2 then
+				ville.nom_ville := fest.ville_festival;
+				ville_list.append (ensVP, ville);
+			end if;
+		end verifie_prog;
+	begin
+		ensFest := festival_io.retrieve (c);
+		festival_list.iterate (ensFest, verifie_prog'Access);
+		if ville_List.is_empty(ensVP) then
+			raise ExAucuneVille;
+		end if;
+	end retrouver_villes_avec_programme;
 
 
-procedure creer_ville (ville : tville) is
-begin
-  ville_io.Save( ville, False );
-exception
- when  GNU.DB.SQLCLI.INTEGRITY_VIOLATION => raise exvilleExiste;
-end creer_ville;
+	procedure creer_ville (ville : tville) is
+	begin
+		ville_io.Save( ville, False );
+	exception
+		when GNU.DB.SQLCLI.INTEGRITY_VIOLATION => raise exvilleExiste;
+	end creer_ville;
 
 
-procedure consultGroupe (nom : unbounded_string; groupe : out tgroupe) is
-begin
-groupe := groupe_io.retrieve_by_pk (nom);
-if groupe_io.is_null (groupe) then raise ExGroupeNonTrouve; end if;
+	procedure consultGroupe (nom : unbounded_string; groupe : out tgroupe) is
+	begin
+		groupe := groupe_io.retrieve_by_pk (nom);
+		if groupe_io.is_null (groupe) then
+			raise ExGroupeNonTrouve;
+		end if;
+	end consultGroupe;
 
-end consultGroupe;
+	procedure consulter_programme_festival(nomville : unbounded_string ; fest : out tfestival; ensProg1, ensProg2 : out Programme_Jour_Festival_List.Vector ) is
+		ensJour : Jour_Festival_List.Vector;
+		j1, j2 : tjour_festival;
+		c1, c2 : db_commons.criteria;
+	begin
+		fest := festival_io.Retrieve_by_pk(nomVille);
+		-- recherche des journées du festival de la ville
+		ensJour := festival_io.Retrieve_Associated_Jour_Festivals( fest);
 
-procedure consulter_programme_festival(nomville : unbounded_string ; fest : out tfestival;  ensProg1, ensProg2 : out Programme_Jour_Festival_List.Vector ) is
-ensJour : Jour_Festival_List.Vector;
-j1, j2 : tjour_festival;
-c1, c2 : db_commons.criteria;
+		j1 := Jour_Festival_List.element(ensJour, Jour_Festival_List.first_index(ensJour));
+		-- recherche du programme de la journée 1, les groupes étant triés par ordre de passage
+		programme_jour_festival_io.Add_Jour_Fest( c1 , j1.id_jour_festival);
+		programme_jour_festival_io.Add_Passage_To_Orderings (c1, asc);
+		ensProg1 := programme_jour_festival_io.retrieve (c1);
 
-begin
-fest := festival_io.Retrieve_by_pk(nomVille);
-  -- recherche des journées du festival de la ville
-ensJour := festival_io.Retrieve_Associated_Jour_Festivals( fest);
-
-j1 := Jour_Festival_List.element(ensJour, Jour_Festival_List.first_index(ensJour));
--- recherche du programme de la journée 1, les groupes étant triés par ordre de passage
-programme_jour_festival_io.Add_Jour_Fest( c1 , j1.id_jour_festival);
-programme_jour_festival_io.Add_Passage_To_Orderings (c1, asc);
-ensProg1 := programme_jour_festival_io.retrieve (c1);
-
-j2 := Jour_Festival_List.element(ensJour, Jour_Festival_List.last_index(ensJour));
--- recherche du programme de la journée 2, les groupes étant triés par ordre de passage
-programme_jour_festival_io.Add_Jour_Fest( c2 , j2.id_jour_festival);
-programme_jour_festival_io.Add_Passage_To_Orderings (c2, asc);
-ensProg2 := programme_jour_festival_io.retrieve (c2);
-
-end consulter_programme_festival;
-
-
+		j2 := Jour_Festival_List.element(ensJour, Jour_Festival_List.last_index(ensJour));
+		-- recherche du programme de la journée 2, les groupes étant triés par ordre de passage
+		programme_jour_festival_io.Add_Jour_Fest( c2 , j2.id_jour_festival);
+		programme_jour_festival_io.Add_Passage_To_Orderings (c2, asc);
+		ensProg2 := programme_jour_festival_io.retrieve (c2);
+	end consulter_programme_festival;
 
 end p_application;
--- src/appli/p_application.ads
+++ src/appli/p_application.ads
@@ -4,33 +4,29 @@ with ada.strings.Unbounded; use ada.strings.Unbounded;
 
 package  p_application is
 
--- vidage des données de la base
-procedure vider_tables ;
+	-- vidage des données de la base
+	procedure vider_tables ;
 
--- enregistre une ville.  
--- lève l'exception ExVilleExiste si elle était déjà présente dans la base
-procedure creer_ville (ville : tville);
+	-- enregistre une ville.  
+	-- lève l'exception ExVilleExiste si elle était déjà présente dans la base
+	procedure creer_ville (ville : tville);
 
--- renvoie toutes les villes enregistrées.
--- lève l'exception ExAucuneVille si le résultat est vide
-procedure retrouver_villes (ensV : out ville_List.Vector) ;
+	-- renvoie toutes les villes enregistrées.
+	-- lève l'exception ExAucuneVille si le résultat est vide
+	procedure retrouver_villes (ensV : out ville_List.Vector) ;
 
--- renvoie toutes les villes dont le festival est entièrement programmé.
--- lève l'exception ExAucuneVille si le résultat est vide
-procedure retrouver_villes_avec_programme (ensVP : out ville_List.Vector );
+	-- renvoie toutes les villes dont le festival est entièrement programmé.
+	-- lève l'exception ExAucuneVille si le résultat est vide
+	procedure retrouver_villes_avec_programme (ensVP : out ville_List.Vector );
 
--- consulte un groupe à partir de son nom.
--- lève l'exception ExGroupeNonTrouve si le groupe ayant ce nom n'est pas présent
-procedure consultGroupe (nom : unbounded_string; groupe : out tgroupe);
+	-- consulte un groupe à partir de son nom.
+	-- lève l'exception ExGroupeNonTrouve si le groupe ayant ce nom n'est pas présent
+	procedure consultGroupe (nom : unbounded_string; groupe : out tgroupe);
 
--- renvoie les programmes des 2 journées du festival de la ville de nom donné
--- la ville nomVille a un festival programmé
-procedure consulter_programme_festival(nomville : unbounded_string ; fest : out tfestival; 
- ensProg1, ensProg2 : out Programme_Jour_Festival_List.Vector );
+	-- renvoie les programmes des 2 journées du festival de la ville de nom donné
+	-- la ville nomVille a un festival programmé
+	procedure consulter_programme_festival(nomville : unbounded_string ; fest : out tfestival; ensProg1, ensProg2 : out Programme_Jour_Festival_List.Vector );
 
-
-ExAucuneVille, ExVilleExiste, ExGroupeNonTrouve : exception;
-
-
+	ExAucuneVille, ExVilleExiste, ExGroupeNonTrouve : exception;
 
 end p_application;
